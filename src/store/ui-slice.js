9167863e6c175ff3f0d056c5162f0fbf7fde96da
enginbukum <yasengin2005@gmail.com>
2023-03-17 03:03:55 +0300

    extrareducers builders

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,45 +1,60 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchCartData, sendCartData } from './cart-actions';
- 
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchCartData, sendCartData } from "./cart-actions";
+
 const uiSlice = createSlice({
-  name: 'ui',
+  name: "ui",
   initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
     },
-  },   
-  extraReducers: {
-    [fetchCartData.rejected]: (state, action) => {
-      state.notification = {
-        status: 'error', 
-        title: 'Error!', 
-        message: action.error.message || 'Fetch failed'
-      };
-    },
-    [sendCartData.pending]: (state) => {
-      state.notification = {
-        status: '', 
-        title: 'Pending ...', 
-        message: 'Sending Books ...'
-      };
-    },
-    [sendCartData.fulfilled]: (state) => {
-      state.notification = {
-        status: 'success', 
-        title: 'Success!', 
-        message: 'Cart data sent successfully!'
-      };
-    },
-    [sendCartData.rejected]: (state, action) => {
-      state.notification = {
-        status: 'error', 
-        title: 'Error!', 
-        message: 'Failed to send cart data!'
-      };
-    }
-  }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCartData.pending, (state) => {
+        state.notification = {
+          status: "",
+          title: "Pending ...",
+          message: "Fetching Books ...",
+        };
+      })
+      .addCase(fetchCartData.fulfilled, (state, action) => {
+        state.notification = {
+          status: "success",
+          title: "Success!",
+          message: "Cart data fetched successfully!",
+        };
+      })
+      .addCase(fetchCartData.rejected, (state, action) => {
+        state.notification = {
+          status: "error",
+          title: "Error!",
+          message: action.error.message || "Fetch failed",
+        };
+      })
+      .addCase(sendCartData.pending, (state) => {
+        state.notification = {
+          status: "",
+          title: "Pending ...",
+          message: "Sending Books ...",
+        };
+      })
+      .addCase(sendCartData.fulfilled, (state) => {
+        state.notification = {
+          status: "success",
+          title: "Success!",
+          message: "Cart data sent successfully!",
+        };
+      })
+      .addCase(sendCartData.rejected, (state, action) => {
+        state.notification = {
+          status: "error",
+          title: "Error!",
+          message: "Failed to send cart data!",
+        };
+      });
+  },
 });
- 
+
 export const uiActions = uiSlice.actions;
 export default uiSlice;
